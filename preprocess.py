08ae8096e89d6fed29f4e6dbf0655f55db0a1ab6
Michal Glomski <132297566+michalglomsky@users.noreply.github.com>
2025-09-11 17:47:51 +0200

    Update preprocess.py
    STAGE 2
    
    OBJECTIVES
    
    In this stage, you will build thefeature_data function with the output of the clean_data function. Inside the feature_data function:
    
    The input parameter is the returned DataFrame from the clean_data function from the previous stage;
    
    Get the unique values in the version column of the DataFrame you got from clean_data as a year (2020, for example) and parse as a datetime object;
    
    Engineer the age feature by subtracting b_day column from version. Calculate the value as year;
    
    Engineer the experience feature by subtracting draft_year column from version. Calculate the value as year;
    
    Engineer the bmi (body mass index) feature from weight (
    w
    w
    ) and height (
    h
    h
    ) columns. The formula is 
    bmi
    =
    w
    /
    h
    2
    bmi=w/h 
    2
     
    ;
    
    Drop the version, b_day, draft_year, weight, and height columns;
    
    Remove the high cardinality features;
    
    Return the modified DataFrame.
    
    EXAMPLE:
    
    path = "../Data/nba2k-full.csv"
    df_cleaned = clean_data(path)
    df = feature_data(df_cleaned)
    print(df[['age', 'experience', 'bmi']].head())
    
    Output:
    
        age	  experience	bmi
    0	36	  17	26.722594
    1	29	   9	25.271652
    2	26	   7	24.662519
    3	32	  13	24.107803
    4	31	  11	25.978759

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import requests
 
@@ -16,19 +17,23 @@ if 'nba2k-full.csv' not in os.listdir('../Data'):
 
 data_path = "../Data/nba2k-full.csv"
 df1 = pd.read_csv(data_path)
-# write your code here
-def clean_data(path):
-    df = pd.read_csv(path)
 
+def clean_data(path):
+
+    df = pd.read_csv(path)
     df['b_day'] = pd.to_datetime(df['b_day'], format='%m/%d/%y')
     df['draft_year'] = pd.to_datetime(df['draft_year'],format='%Y')
     df.fillna({'team':'No Team'},inplace=True)
+
     # example height entry: 6-9 / 2.06
     df['height'] = df['height'].str.split().str[-1].astype(float)
+
     # example weight entry: 250 lbs. / 113.4 kg.
     df['weight'] = df['weight'].str.split('/').str[-1].str.split().str[0].astype(float)
+
     # example salary entry: $12500000
     df['salary'] = df['salary'].str.replace('$', '').astype(float)
+
     # If outside the USA type as Not-USA
     df['country'] = df['country'].apply(lambda x: 'Not-USA' if x != 'USA' else 'USA')
     df['country'] = df['country'].apply(lambda x: 'Not-USA' if x != 'USA' else 'USA')
@@ -36,7 +41,33 @@ def clean_data(path):
 
     return df
 
-"""df2 = clean_data(data_path)
-print(df1.head())
-print(df1['height'])
-print(df2.head())"""
+def feature_data(df):
+
+    # Series object which has 'versions' entries in 2000 + %y format
+    version_to_time = pd.Series(df['version'].str.extract('(\d\d$)').iloc[:, 0].astype(int) + 2000)
+
+    # Change unique values in a version column to datetime object
+    unique_values = pd.to_datetime(version_to_time.unique(), format='%Y')
+
+    # Engineer the age feature by subtracting b_day column from version. Calculate the value as year;
+    df['age'] = np.ceil((pd.to_datetime(version_to_time, format='%Y') - pd.to_datetime(df['b_day'])).dt.days / 365.25)
+
+    # Engineer the experience feature by subtracting draft_year column from version. Calculate the value as year;
+    df['experience'] = np.ceil((pd.to_datetime(version_to_time, format='%Y') - pd.to_datetime(df['draft_year'])).dt.days / 365.25)
+
+    # Engineer bmi feature representing the bmi of a player
+    df['bmi'] = df.weight / np.pow(df.height, 2)
+
+    # Drop 'version', 'b_day', 'draft_year', 'weight', 'height' columns
+    df = df.drop(['version', 'b_day', 'draft_year', 'weight', 'height'], axis=1)
+
+    # Drop high cardinality features (>50 unique values)
+    high_cardinality_cols = [col for col in df.columns if df[col].dtype == 'object' and df[col].nunique() > 50]
+    df = df.drop(columns=high_cardinality_cols)
+
+    return df
+
+if __name__ == "__main__":
+    df_cleaned = clean_data(data_path)
+    df = feature_data(df_cleaned)
+    print(df[['age', 'experience', 'bmi']].head())
